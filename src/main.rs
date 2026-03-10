228e6c1f396f9e8d6357291b338905aacdde9526
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-10 18:23:46 +0530

    add: prettify and minify capabalities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use serde_json::{to_string, to_string_pretty};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+    path : PathBuf,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Pretty,
+    Minify,
+    Get{
+        path : String
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let file_value = std::fs::read_to_string(cli.path).expect("Failed to read file");
+
+    let file_json = serde_json::from_str::<serde_json::Value>(file_value.as_str()).expect("expect a json file");
+    
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Pretty=>{
+    let pretty_string_json = to_string_pretty(&file_json).expect("Expected JSON file");
+            println!("prettify the code {}", pretty_string_json);
+        },
+        Commands::Get{path}=>{
+            println!("get somehting in the code");
+        },
+        Commands::Minify=>{
+            let minify = to_string(&file_json).expect("bas aise hi expect msg likhna toh likh rha hu guys");
+            println!("minify the code {}", minify);
+        }
+    }
+
+    // Continued program logic goes here...
 }
